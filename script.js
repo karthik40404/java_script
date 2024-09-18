2da955267a887cdbee3d296e99e3110a490b2a12
karthik40404 <karpython123@gmail.com>
2024-09-18 11:27:27 +0530

    demo

--- script.js
+++ script.js
@@ -12,7 +12,18 @@
 // const a=20
 // alert(a)
 
-let a="qwer"
-let b=null
-alert(typeof b)
+// let a="qwer"
+// let b=null
+// alert(typeof b)
 
+// let a=2
+// a+=10
+// a-=10
+// a/=10
+// a*=10
+// a%=10
+// alert(a)
+
+let a=10
+let b=10
+alert(a!==b)
