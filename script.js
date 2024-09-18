954907357259769bf6d5ecc14d59e97b31b93572
karthik40404 <karpython123@gmail.com>
2024-09-18 11:02:53 +0530

    new

---
+++ script.js
@@ -0,0 +1,18 @@
+//  alert("who are you")
+
+// var a
+// a=10
+// alert(a)
+
+// let a =10
+// a=20
+// alert(a)
+
+// const a=10
+// const a=20
+// alert(a)
+
+let a="qwer"
+let b=null
+alert(typeof b)
+
