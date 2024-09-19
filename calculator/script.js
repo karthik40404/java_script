ea9f25e24e8f0c12cc14ab3cb87c318223e69733
karthik40404 <karpython123@gmail.com>
2024-09-19 23:17:31 +0530

    ele

---
+++ calculator/script.js
@@ -0,0 +1,67 @@
+function sam1(){
+    a=parseInt(document.getElementById('fno').value)
+    b=parseInt(document.getElementById('sno').value)
+    console.log(a)
+    console.log(b)
+    document.getElementById("h1").innerHTML=a+b
+}
+function sam2(){
+    a=parseInt(document.getElementById('fno').value)
+    b=parseInt(document.getElementById('sno').value)
+    console.log(a)
+    console.log(b)
+    document.getElementById("h1").innerHTML=a-b
+}
+function sam3(){
+    a=parseInt(document.getElementById('fno').value)
+    b=parseInt(document.getElementById('sno').value)
+    console.log(a)
+    console.log(b)
+    document.getElementById("h1").innerHTML=a*b
+}
+function sam4(){
+    a=parseInt(document.getElementById('fno').value)
+    b=parseInt(document.getElementById('sno').value)
+    console.log(a)
+    console.log(b)
+    document.getElementById("h1").innerHTML=a/b
+}
+function sam5(){
+    a=parseInt(document.getElementById('fno').value)
+    b=parseInt(document.getElementById('sno').value)
+    console.log(a)
+    console.log(b)
+    document.getElementById("h1").innerHTML=a%b
+}
+function sam6(){
+    a=parseInt(document.getElementById('fno').value)
+    b=parseInt(document.getElementById('sno').value)
+    if(a>b){
+        document.getElementById("h1").innerHTML=a
+    }
+    else{
+        document.getElementById("h1").innerHTML=b
+    }
+}
+function sam7(){
+    a=parseInt(document.getElementById('fno').value)
+    b=parseInt(document.getElementById('sno').value)
+    c=parseInt(document.getElementById('tno').value)
+    if(a>b && a>c){
+        document.getElementById("h1").innerHTML=a
+    }
+    else if (b>c && b>a) {
+        document.getElementById("h1").innerHTML=b  
+    }
+    else{
+        document.getElementById("h1").innerHTML=c
+    }
+}
+// function sam7() {
+//     var a = parseInt(document.getElementById('fno').value);
+//     var b = parseInt(document.getElementById('sno').value);
+//     var c = parseInt(document.getElementById('tno').value);
+    
+//     var maxNum = Math.max(a, b, c);
+//     document.getElementById("h1").innerHTML = maxNum;
+// }
---
+++ demo2/script.js
@@ -0,0 +1,11 @@
+function sam(){
+    alert('welcome')
+    alert('welcome1')
+}
+function sam1(){
+    a=parseInt(document.getElementById('fno').value)
+    b=parseInt(document.getElementById('sno').value)
+    console.log(a)
+    console.log(b)
+    alert(a+b)
+}
