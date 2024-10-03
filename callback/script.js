cf91ebcf4dd8a9edf14bfac5207c1e022ff056f7
karthik40404 <karpython123@gmail.com>
2024-10-03 10:19:17 +0530

    boom

--- callback/script.js
+++ callback/script.js
@@ -14,18 +14,56 @@
 // display(display1)
 // console.log('end');
 
-function display(){
-    // console.log('display');
-}
-const a=setInterval(display,2000)
+// function display(){
+// }
+// const a=setInterval(display,2000)
 
-let count=10
-let b=setInterval(function(){
-    console.log(count);
-    count--
-    if(count==0){
-        clearInterval(b)
-        console.log('finish');
+// let count=10
+// let b=setInterval(function(){
+//     console.log(count);
+//     count--
+//     if(count==0){
+//         clearInterval(b)
+//         console.log('finish');
         
+//     }
+// },2000)
+
+// function display(){
+//     const main=document.querySelector('.main')
+//     const div=document.createElement('div')
+//     let count=10
+//     let c=setInterval(function(){
+//     div.innerHTML=`<h2>00:${count}</h2>`
+//     count--
+//     if(count==0){
+//         clearInterval(c)
+//         div.innerHTML=`<h2 style='color: red'>finish</h2>`
+//         alert('BOOM')
+//     }
+//     },1000)  
+//      main.appendChild(div)
+// }
+// display()
+
+function demo(){
+    const main=document.querySelector('.main')
+    const div=document.createElement('div')
+    let c1=3
+    let c2=2
+    let c=setInterval(function(){
+        div.innerHTML=`<h2>${c1}:0${c2}</h2>`
+        c2--
+        if(c2==-1){
+            c1--
+            c2=2
+        }
+        if(c1==0){
+            clearInterval(c)
+            div.innerHTML=`<h2>complete</h2>`
+        }
+        
+    },1000);
+    main.appendChild(div)
     }
-},2000)
+    demo()
